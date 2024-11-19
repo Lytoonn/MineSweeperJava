c74b34308cdfc9866f51507936ded773ea02f223
Lytoon <lytoonytgg@gmail.com>
2024-11-19 10:40:22 +0000

    Default Leaderboard and Optimize menus

--- src/classes/Player.java
+++ src/classes/Player.java
@@ -1,17 +1,20 @@
 package classes;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class Player {
 
     public Player(Board brd){
         this.brd = brd;
+        LoadTable();
     }
 
     private Scanner sc;
     private Board brd;
     private boolean isOver = false;
     private String nickName;
+    private String[] top10Wins = new String[10];
 
     public void game(){
         sc = new Scanner(System.in);
@@ -66,6 +69,42 @@ public class Player {
         return aux;
     }
 
-    //PLAYER COMMANDS
+    private void LoadTable() {
+        File file = new File("Top10.dat");
+        if (!file.exists()) {
+            System.out.println("Top10.dat not found. Initializing default leaderboard.");
+            for (int i = 0; i < top10Wins.length; i++) {
+                top10Wins[i] = "No record";
+            }
+            return;
+        }
 
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+            top10Wins = (String[]) ois.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void SaveTable(){
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("Top10.dat"))) {
+            oos.writeObject(top10Wins);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void printTop(){
+        System.out.println("------ Top 10 ------");
+        for(int i = 0; i < top10Wins.length; i++){
+            System.out.println("| "+(i+1)+". "+top10Wins[i] + "       |");
+        }
+        System.out.println("|____________________|");
+    }
+
+    public void enterContinue(){
+        System.out.println("Pressione enter para continuar...");
+        sc = new Scanner(System.in);
+        sc.nextLine();
+    }
 }
--- src/start/Main.java
+++ src/start/Main.java
@@ -3,22 +3,67 @@ package start;
 import classes.Board;
 import classes.Player;
 
-public class Main {
-    public static void main(String[] args) {
-        Board brd = new Board();
-        Player plr = new Player(brd);
+import java.util.Scanner;
 
-        plr.game();
+public class Main {
+
+    private static Player plr;
+    private static Board brd;
+
+    public static void main(String[] args) {
+        brd = new Board();
+        plr = new Player(brd);
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            String aux = null;
+            int choice;
+
+            InitialMenu();
+            do{
+                aux = sc.nextLine();
+                try{
+                    choice = Integer.parseInt(aux);
+                }catch(NumberFormatException e){
+                    System.out.println("Please enter a valid Input.");
+                    choice = -1;
+                }
+            }while(choice <= 0 && choice > 4);
+
+            switch(choice) {
+                case 1 -> plr.game();
+                case 2 -> {
+                    plr.printTop();
+                    plr.enterContinue();
+                }
+                case 3 -> credits();
+                case 4 -> System.exit(0);
+            }
+        }
     }
 
-    private void InitialMenu(){
-        System.out.print(" ____________________\n");
-        System.out.print("| MineSweeper Game   |\n");
-        System.out.print("|--------------------|\n");
-        System.out.print("| 1. New Game        |\n");
-        System.out.print("| 2. Last 10 Wins    |\n");
-        System.out.print("| 3. Credits         |\n");
-        System.out.print("| 4. Exit Game       |\n");
-        System.out.print("|____________________|\n");
+    private static void InitialMenu(){
+        System.out.print("""
+                 ____________________
+                | MineSweeper Game   |
+                |--------------------|
+                | 1. New Game        |
+                | 2. Last 10 Wins    |
+                | 3. Credits         |
+                | 4. Exit Game       |
+                |____________________|
+                """);
+    }
+
+    private static void credits(){
+        System.out.print("""
+                 ____________________
+                | MineSweeper Game   |
+                |--------------------|
+                |       Made By      |
+                |       Lytoon       |
+                |____________________|
+                """);
+        plr.enterContinue();
     }
 }
